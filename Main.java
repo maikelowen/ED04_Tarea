a521800f80f7f6c0d3c9944792ebbc5046d8606a
Miguel Portilla <51139022+maikelowen@users.noreply.github.com>
2021-02-10 18:57:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,13 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        operativa_cuenta();
+        operativa_cuenta(new Operativa_cuentaParameter());
     }
 
-	private static void operativa_cuenta() {
+	private static void operativa_cuenta(Operativa_cuentaParameter cantidad) {
 		CCuenta cuenta1;
 		double saldoActual;
+		
 		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
